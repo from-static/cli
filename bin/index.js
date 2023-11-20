75fc1cc7008f260be8ed12654c1802a044808731
Joe Bottigliero <694253+jbottigliero@users.noreply.github.com>
2023-11-19 19:01:50 -0600

    chore: initial commit

---
+++ bin/index.js
@@ -0,0 +1,68 @@
+#!/usr/bin/env node
+
+const { mkdirSync, rmdirSync, existsSync, readFileSync, writeFileSync } = require('node:fs');
+const { join } = require('node:path');
+const { execSync } = require('node:child_process');
+
+const RENDERERS = {
+  RESUME: {
+    id: 'RESUME',
+    repository: 'git@github.com:from-static/static-resume.git'
+  },
+  RESEARCH_DATA_PORTAL: {
+    id: 'RESEARCH_DATA_PORTAL',
+    repository: 'git@github.com:from-static/static-research-data-portal.git'
+  }
+}
+
+function getRenderer(type) {
+  return RENDERERS[type?.toUpperCase()];
+}
+
+
+require('yargs/yargs')(process.argv.slice(2))
+  .command('build', 'build from static.json', (yargs) => {
+    yargs.option('path', {
+      type: 'string',
+      default: 'static.json',
+      describe: 'path to the static.json file to processs'
+    })
+  }, (argv) => {
+    const staticExists = existsSync(argv.path);
+    if (!staticExists) {
+      throw Error(`Unable to locate ${argv.path}`);
+    }
+
+    const STATIC = JSON.parse(readFileSync(argv.path));
+
+    const workspace = join(__dirname, '..', 'workspace');
+
+    const exists = existsSync(workspace);
+    if (exists) {
+      rmdirSync(workspace, {
+        recursive: true
+      });
+    }
+
+    mkdirSync(workspace);
+
+    const renderer = getRenderer(STATIC.type);
+
+    execSync(`cd ${workspace} && git clone ${renderer.repository} . && npm ci`);
+
+    writeFileSync(join(workspace, 'static.json'), JSON.stringify(STATIC));
+
+    execSync(`cd ${workspace} && npm run build`);
+
+  })
+  .help()
+  .argv
+
+
+
+
+
+
+
+
+
